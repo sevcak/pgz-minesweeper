4b683936cfdbcfff22e33b22e050e6bbd5bca4db
Martin Kockar <106703108+mkockar@users.noreply.github.com>
2022-11-01 20:43:04 +0000

    Pridal som vykreslovanie policok do pola

--- main.py
+++ main.py
@@ -7,9 +7,19 @@ TITLE = 'Minesweeper'
 
 #TEST
 
+policka = list()
+for i in range(20):
+  for j in range(9):
+    policko = Actor('tile', topleft = (10 + i*31, 71 + j * 31))
+    policka.append(policko)
+
 def draw():
     # background fill
     screen.blit('bg', (0,0))
+  
+    for policko in policka:
+      policko.draw()
+    
 
 def update():
     pass
